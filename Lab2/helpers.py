60cace6193992377d3eef4d7a4df27f28e0ca983
Mieciu <mati.lewandowski@gmail.com>
2023-03-12 11:45:23 +0100

    Nowe zaminy

--- Lab2/helpers.py
+++ Lab2/helpers.py
@@ -1,3 +1,6 @@
+import random
+from Lab1.helpers import convert_from_adjacency_matrix_to_adjacency_list
+
 # Ad. 1
 def is_graphical(sequence):
     sequence = sorted(sequence, reverse=True)
@@ -12,12 +15,58 @@ def is_graphical(sequence):
     return True
 
 # return type should be one of the types from witch we can convert to adj_matrix
-def constuct_graph(sequnace):
-    pass
+def construct_graph(sequence):
+    # Sort the sequence in non-increasing order
+    seq = sorted(sequence, reverse=True)
+
+    if not is_graphical(sequence):
+        return None
+
+    # Create the adjacency matrix
+    n = len(seq)
+    adj_matrix = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1, n):
+            if seq[i] > 0 and seq[j] > 0:
+                seq[i] -= 1
+                seq[j] -= 1
+                adj_matrix[i][j] = adj_matrix[j][i] = 1
+
+    return adj_matrix
 
 # Ad. 2
-def randomize_graph(sequance):
-    pass
+def randomize_graph(sequence, num_iterations):
+    adj_matrix = construct_graph(sequence)
+    graph = convert_from_adjacency_matrix_to_adjacency_list(adj_matrix)
+
+    # print(adj_matrix)
+    # for _ in range(numberOfRandomisations):
+    #     pass
+
+    randomized_graph = dict(graph)  # create a copy of the original graph
+    num_vertices = len(graph)
+
+    for iteration in range(num_iterations):
+        # randomly choose two edges to swap
+        u, v = random.sample(range(num_vertices), 2)
+        u_neighbors = randomized_graph[u]
+        v_neighbors = randomized_graph[v]
+
+        # choose a random vertex from each list
+        u_neighbor = random.choice(u_neighbors)
+        v_neighbor = random.choice(v_neighbors)
+
+        # make sure the new edges don't already exist
+        if u_neighbor != v and v_neighbor != u and u_neighbor not in v_neighbors and v_neighbor not in u_neighbors:
+            # swap the edges
+            u_neighbors.remove(u_neighbor)
+            u_neighbors.append(v_neighbor)
+            v_neighbors.remove(v_neighbor)
+            v_neighbors.append(u_neighbor)
+            randomized_graph[u] = u_neighbors
+            randomized_graph[v] = v_neighbors
+
+    return randomized_graph
 
 # Ad. 3
 def find_connected_components(sequnace):
@@ -27,7 +76,7 @@ def find_connected_components(sequnace):
 def create_random_Eulerian_graph():
     pass
 
-def find_Eulerian_cycle(sequance):
+def find_Eulerian_cycle(sequence):
     pass
 
 # Ad. 5
@@ -35,5 +84,5 @@ def generate_random_k_regular_graph():
     pass
 
 # Ad. 6
-def is_Hamiltionian(sequance):
+def is_Hamiltionian(sequence):
     pass
--- Lab2/main.py
+++ Lab2/main.py
@@ -7,4 +7,7 @@ from helpers import *
 graphical_sequance = [4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2]
 non_graphical_sequance = [4, 4, 3, 1, 2]
 
+# Zad1
 print(f"Graphical: {is_graphical(graphical_sequance)}\nNon graphical: {is_graphical(non_graphical_sequance)}")
+draw_graph(construct_graph(graphical_sequance))
+print(randomize_graph(graphical_sequance, 5))
