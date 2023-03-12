86c8b69170a720cfeb20b07ce79837f513695a6b
Nikodem <nikodemszwast@gmail.com>
2023-03-12 13:48:15 +0100

    add randomize_graph function

--- Lab2/helpers.py
+++ Lab2/helpers.py
@@ -1,3 +1,7 @@
+import random
+import numpy as np
+from Lab1.helpers import *
+
 # Ad. 1
 def is_graphical(sequence):
     sequence = sorted(sequence, reverse=True)
@@ -12,16 +16,96 @@ def is_graphical(sequence):
     return True
 
 # return type should be one of the types from witch we can convert to adj_matrix
-def constuct_graph(sequnace):
-    pass
+def construct_graph(sequence):
+    # Sort the sequence in non-increasing order
+    seq = sorted(sequence, reverse=True)
+
+    if not is_graphical(sequence):
+        raise ValueError("Sekwenchja nie jest graficzna")
+
+    # Create the adjacency matrix
+    n = len(seq)
+    adj_matrix = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1, n):
+            if seq[i] > 0 and seq[j] > 0:
+                seq[i] -= 1
+                seq[j] -= 1
+                adj_matrix[i][j] = adj_matrix[j][i] = 1
+
+    return adj_matrix
 
 # Ad. 2
-def randomize_graph(sequance):
-    pass
+def randomize_graph(sequence, num_iterations):
+    adj_matrix = construct_graph(sequence)
+    graph = convert_from_adjacency_matrix_to_adjacency_list(adj_matrix)
+    keyList = range(1, len(graph) + 1)
+    
+    # initialize dictionary
+    randomized_graph = {}
+    
+    # iterating through the elements of list
+    for i in keyList:
+        randomized_graph[i] = graph[i-1]
+    
+    num_vertices = len(graph)
+
+    for _ in range(num_iterations):
+        # randomly choose two edges to swap
+        u, v = random.sample(range(1, num_vertices + 1), 2)
+        u_neighbors = randomized_graph[u]
+        v_neighbors = randomized_graph[v]
+
+        # choose a random vertex from each list
+        u_neighbor = random.choice(u_neighbors)
+        v_neighbor = random.choice(v_neighbors)
+        if u_neighbor != v and v_neighbor != u and u_neighbor not in v_neighbors and v_neighbor not in u_neighbors:
+            u_neighbors.remove(u_neighbor)
+            u_neighbors.append(v_neighbor)
+            v_neighbors.remove(v_neighbor)
+            v_neighbors.append(u_neighbor)
+            randomized_graph[u] = u_neighbors 
+            randomized_graph[v] = v_neighbors
+
+            u_neighbors_neighbours = randomized_graph[u_neighbor]
+            v_neighbors_neighbours = randomized_graph[v_neighbor]
+
+            # if v_neighbor not in u_neighbors_neighbours and u not in v_neighbors_neighbours:
+            u_neighbors_neighbours.append(v)
+            u_neighbors_neighbours.remove(u)
+            v_neighbors_neighbours.append(u)
+            v_neighbors_neighbours.remove(v)
+            randomized_graph[u_neighbor] = u_neighbors_neighbours
+            randomized_graph[v_neighbor] = v_neighbors_neighbours
+        
+    adj_list = []
+    for value in randomized_graph.values():
+        adj_list.append(value)
+
+    adj_matrix = convert_from_adjacency_list_to_adjacency_matrix(adj_list)
+    upper_values = np.tril(adj_matrix)
+    symmetric_matrix = upper_values + upper_values.T
+    return symmetric_matrix
 
 # Ad. 3
-def find_connected_components(sequnace):
-    pass
+def find_largest_connected_component(graph): #graph to dictionary (lista sasiedztwa) trzeba przerobic 
+    visited = set()
+    largest_component = []
+    for v in graph:
+        if v not in visited:
+            component = []
+            dfs(graph, visited, v, component)
+            if len(component) > len(largest_component):
+                largest_component = component
+    return largest_component
+
+def dfs(graph, visited, v, component):
+    visited.add(v)
+    component.append(v)
+    for neighbor in graph[v]:
+        if neighbor not in visited:
+            dfs(graph, visited, neighbor, component)
+
 
 # Ad. 4
 def create_random_Eulerian_graph():
--- Lab2/main.py
+++ Lab2/main.py
@@ -7,4 +7,9 @@ from helpers import *
 graphical_sequance = [4, 2, 2, 3, 2, 1, 4, 2, 2, 2, 2]
 non_graphical_sequance = [4, 4, 3, 1, 2]
 
-print(f"Graphical: {is_graphical(graphical_sequance)}\nNon graphical: {is_graphical(non_graphical_sequance)}")
+# print(f"Graphical: {is_graphical(graphical_sequance)}\nNon graphical: {is_graphical(non_graphical_sequance)}")
+
+adj_matrix = construct_graph(graphical_sequance)
+draw_graph(adj_matrix)
+randomized_graph = randomize_graph(graphical_sequance, 10)
+draw_graph(randomized_graph)
