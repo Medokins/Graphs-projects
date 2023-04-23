8f3119333a356289c29fb6585cae53d9a0cad71d
Nikodem <nikodemszwast@gmail.com>
2023-04-23 14:20:21 +0200

    add exercise number addnotation in prints

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,6 +1,7 @@
 from helpers import *
 
 # Ad.1
+print("Ad. 1")
 print('adjacency_matrix:')
 adjacency_matrix = read_data("adjacency_matrix.txt")
 print_data(adjacency_matrix)
@@ -12,10 +13,9 @@ print_adj_list(adjacency_list)
 print('adjacency_matrix to incidence_matrix')
 incidence_matrix = convert_from_adjacency_matrix_to_incidence(adjacency_matrix)
 print_data(incidence_matrix)
-
-print('='*40)
 # ==========================
 
+print("\nAd. 2")
 print('adjacency_list:')
 adjacency_list = read_data("adjacency_list.txt")
 print_adj_list(adjacency_list)
@@ -27,10 +27,9 @@ print_data(adjacency_matrix)
 print('adjacency_list to incidence_matrix')
 incidence_matrix = convert_from_adjacency_list_to_incidence(adjacency_list)
 print_data(incidence_matrix)
-
-print('='*40)
 # ==========================
 
+print("\nAd. 3")
 print('incidence_matrix:')
 incidence_matrix = read_data("incidence_matrix.txt")
 print_data(incidence_matrix)
@@ -42,15 +41,15 @@ print_data(adjacency_matrix)
 print('incidence_matrix to adjacency_list')
 adjacency_list = convert_from_incidence_to_adjacency_list(incidence_matrix)
 print_adj_list(adjacency_list)
-
-print('='*40)
 # ==========================
 
-# Ad.2
+# Ad.4
+print("\nAd. 4")
 adjacency_matrix = read_data("adjacency_matrix.txt")
 draw_graph(adjacency_matrix)
 
-# Ad.3
+# Ad.5
+print("\nAd. 5")
 adj_matrix = generate_random_graph(10, 25)
 draw_graph(adj_matrix)
 adj_matrix = generate_random_graph_2(10, 0.5)
